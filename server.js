287b42c1e16e9200566f40df7ed2f002cca6f3a1
Harshit Mishra <harshitmishra2011@gmail.com>
2023-01-25 21:58:17 +0530

    added databse config and initialization

--- server.js
+++ server.js
@@ -3,6 +3,24 @@ const cors = require('cors')
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(`${process.env.mongodb}/fileManager`)
+
+app.get("/api/health", function(req, res){
+    res.send(`Serer active at time: ${new Date()}`)
+})
+
+
+
+app.listen(process.env.PORT, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`);
+    }
+})
